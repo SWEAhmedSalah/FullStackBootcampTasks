85c9ab271898fbd7c8b96aebd231519f8c42d8ac
SWEAhmedSalah <ahmed.salahfci1164@hotmail.com>
2026-06-14 07:55:03 -0400

    code added

---
+++ tasks/task-01/starter/src/com/app/Draw3d.java
@@ -0,0 +1,11 @@
+package com.app;
+
+public class Draw3d {
+    public void drawCircle() {
+        System.out.println("Drawing a 3d for Circle");
+    }
+
+    public void drawSquare() {
+        System.out.println("Drawing a 3d for Square");
+    }
+}
---
+++ tasks/task-01/starter/src/com/app/Main3.java
@@ -0,0 +1,10 @@
+//package com.app;
+//
+//public class Main3
+//{
+//	public static void main(String[] args)
+//	{
+//       Circle circle = new Circle(new Draw2d());
+//       circle.draw();
+//    }
+//}
